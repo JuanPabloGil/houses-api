6783b0bda158ab24e4964db1cbe796e687b70d92
Juan Pablo <jpablomgil@gmail.com>
2020-07-20 14:21:46 -0500

    Change direction of cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(Rails.env.production? ? 'https://auth-api-front.herokuapp.com' : 'http://localhost:3000')
+    origins(Rails.env.production? ? 'https://house-api-jpmg.herokuapp.com/' : 'http://localhost:3000')
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end

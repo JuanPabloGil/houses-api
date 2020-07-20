5aada7f192b72f2dff9580c1d0cf7ceb96351ec4
Juan Pablo <jpablomgil@gmail.com>
2020-07-20 15:01:24 -0500

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(Rails.env.production? ? 'https://house-api-jpmg.herokuapp.com/' : 'http://localhost:3000')
-    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    origins  (Rails.env.production? ? 'https://house-api-jpmg.herokuapp.com' : 'http://localhost:3000')
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
-end
+
+end   
